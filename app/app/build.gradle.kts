463e06bd636ea1322c660ce14a1676a364063c6a
bingyenufe <121657961@163.com>
2026-09-06 00:50:53 +0800

    fix(build): simplify buildTypes and add pipefail error trapping

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -20,11 +20,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-            signingConfig = signingConfigs.findByName("debug")
+            signingConfig = signingConfigs.getByName("debug")
         }
         debug {
             isMinifyEnabled = false
