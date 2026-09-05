525d7df0ce600e9853067ea839468336dc19b256
bingyenufe <121657961@163.com>
2026-09-06 00:34:01 +0800

    fix(build): ensure signingConfig and build debug/release targets

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -24,7 +24,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.findByName("debug")
+        }
+        debug {
+            isMinifyEnabled = false
         }
     }
     compileOptions {
